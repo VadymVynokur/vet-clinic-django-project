4ad2bfc5a10e3db83b0a90ab5461673b52a8dd23
Vadym Vynokur <king.krollie@gmail.com>
2025-12-23 16:37:26 +0200

    deploy

--- manage.py
+++ manage.py
@@ -3,10 +3,16 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vet_clinic_django_project.settings')
+    load_dotenv()
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        os.getenv("DJANGO_SETTINGS_MODULE")
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
